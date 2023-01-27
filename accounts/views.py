4f9242cde1c7b0ca4ca0d12bb6c2492d50a98c9f
iceface <www.iceface@gmail.com>
2023-01-27 16:48:03 +0630

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views import generic
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
+
+class SignupPageView(generic.CreateView):
+	form_class = CustomUserCreationForm
+	success_url = reverse_lazy('login')
+	template_name = "registration/singup.html"
---
+++ books/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
+class BookListView(LoginRequiredMixin ,ListView):
+	model = Book
+	context_object_name = "book_list"
+	template_name = "books/book_list.html"
+	login_url = 'login'
+
+
+class BookDetailView(
+	LoginRequiredMixin,
+	PermissionRequiredMixin,
+	DetailView):
+
+	model = Book
+	context_object_name = 'book'
+	template_name = 'books/book_detail.html'
+	login_url = 'login'
+	permission_required = 'books.special_status'
